523032b9fc958b6eb2e03201fc5dc944f2b57390
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 22:18:01 -0300

    normalized develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { Header } from "./components/header";
-import { WorkoutCard } from "./components/workout-card";
-
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <WorkoutCard
-          id="1"
-          title="FitDance"
-          date="20/02/2026"
-          durationMinutes={60}
-          intensity={4}
-        />
-        <WorkoutCard
-          id="2"
-          title="Zumba"
-          date="21/02/2026"
-          durationMinutes={45}
-          intensity={5}
-        />
-      </main>
+     <h1>trackfit</h1>
     </>
-  );
-}
-
-export default App;
+  )
+  export default App;
